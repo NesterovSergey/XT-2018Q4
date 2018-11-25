ea5b7c30cd7cc186a77d07d892d924da8d565df4
Sergey Nesterov <RL666@bk.ru>
2018-11-25 19:08:09 +0400

    Logic fixes in FontAdjustment task

--- Epam.Task2/Epam.Task2.FontAdjustment/Program.cs
+++ Epam.Task2/Epam.Task2.FontAdjustment/Program.cs
@@ -4,44 +4,63 @@ namespace Epam.Task2.FontAdjustment
 {
     class Program
     {
+        [Flags]
+        public enum Options
+        {
+            italic = 1,
+            bold = 2,
+            underline = 4,
+        }
+
         static void Main(string[] args)
         {
+            Options currentFont = 0;
             int x;
-            bool bold = false;
-            bool italic = false;
-            bool underline = false;
+            bool italic = true;
+            bool bold = true;
+            bool underline = true;
+
+            Console.Write($"Current font parameters: None{Environment.NewLine}" +
+                          $"Enter: {Environment.NewLine}" +
+                          $"1: italic {Environment.NewLine}" +
+                          $"2: bold {Environment.NewLine}" +
+                          $"3: underline {Environment.NewLine}" +
+                          $"4: exit {Environment.NewLine}");
 
             while (true)
             {
-                Console.WriteLine("\nВведите: \n 1: bold \n 2: italic \n 3: underline");
-                if (int.TryParse(Console.ReadLine(), out x))
+                x = int.Parse(Console.ReadLine());
+                switch (x)
                 {
-                    if (x == 1)
-                    {
-                        bold = !bold;
-                    }
-                    if (bold)
-                    {
-                        Console.Write("bold ");
-                    }
-
-                    if (x == 2)
-                    {
+                    case 1:
+                        currentFont = italic ? currentFont + 1 : currentFont - 1;
                         italic = !italic;
-                    }
-                    if (italic)
-                    {
-                        Console.Write("italic ");
-                    }
-
-                    if (x == 3)
-                    {
+                        break;
+                    case 2:
+                        currentFont = bold ? currentFont + 2 : currentFont - 2;
+                        bold = !bold;
+                        break;
+                    case 3:
+                        currentFont = underline ? currentFont + 4 : currentFont - 4;
                         underline = !underline;
-                    }
-                    if (underline)
-                    {
-                        Console.Write("underline ");
-                    }
+                        break;
+                    case 4:
+                        currentFont -= 10;
+                        break;
+                    default:
+                        Console.WriteLine("This option is not exist");
+                        break;
+                }
+
+                if (currentFont > 0)
+                {
+                    Console.Write("Current font parameters: ");
+                    Console.WriteLine(currentFont);
+                }
+                else if (currentFont == 0)
+                {
+                    Console.Write("Current font parameters: ");
+                    Console.WriteLine("None");
                 }
                 else
                 {
