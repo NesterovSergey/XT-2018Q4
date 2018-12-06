a5aa06535cce5f0a50000280b34fbbd919212e5b
Sergey Nesterov <RL666@bk.ru>
2018-12-06 05:03:26 +0400

    Added Epam.Task3.user and updated gitignore

---
+++ Epam.Task3/Epam.Task3.User/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Epam.Task3.User
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User user = new User();
+
+            string name;
+            string lastName;
+            string secondName;
+
+            DateTime date;
+            int day;
+            int month;
+            int year;
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name: ");
+                name = Console.ReadLine();
+                try
+                {
+                    user.Name = name;
+
+                    break;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter your second name: ");
+                secondName = Console.ReadLine();
+                try
+                {
+                    user.SecondName = secondName;
+
+                    break;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter your last name: ");
+                lastName = Console.ReadLine();
+                try
+                {
+                    user.LastName = lastName;
+
+                    break;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter day of your birth: ");
+                day = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter month of your birth: ");
+                month = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter year of your birth: ");
+                year = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    date = new DateTime(year, month, day);
+                    user.DateOfBirth = date;
+                    break;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("Name: {0}", user.Name);
+            Console.WriteLine("Second name: {0}", user.SecondName);
+            Console.WriteLine("Last name: {0}", user.LastName);
+            Console.WriteLine("Date of Birth: {0:d}", user.DateOfBirth);
+            Console.WriteLine("Age: {0}", user.Age);
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.User/User.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Epam.Task3.User
+{
+    public class User
+    {
+        private string name;
+        private string lastName;
+        private string secondName;
+        private int age;
+        private DateTime dateOfBirth;
+        private DateTime now = DateTime.Now;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (NameCheck(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect name");
+                }
+            }
+        }
+
+        public string SecondName
+        {
+            get
+            {
+                return secondName;
+            }
+
+            set
+            {
+                if (NameCheck(value))
+                {
+                    secondName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect second name");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                if (NameCheck(value))
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect last name");
+                }
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+
+            set
+            {
+                dateOfBirth = value;
+
+                age = now.Year - dateOfBirth.Year;
+
+                if (dateOfBirth.Month >= now.Month && dateOfBirth.Day >= now.Day)
+                {
+                    age++;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                if (age != 0)
+                {
+                    return age;
+                }
+                else
+                {
+                    throw new Exception("You did not enter the date of birth");
+                }
+            }
+        }
+
+        private bool NameCheck(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!char.IsLetter(name[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
