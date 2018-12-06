ce9581f0ca4467a09984c147c69ace98868aa236
Sergey Nesterov <RL666@bk.ru>
2018-12-06 04:49:59 +0400

    Added Epam.Task3.Round

---
+++ Epam.Task3/Epam.Task3.Round/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Epam.Task3.Round
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Round round = new Round();
+
+            Console.WriteLine("Enter X coordinate: ");
+            round.X = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Y coordinate: ");
+            round.Y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a radius: ");
+            while (true)
+            {
+                Console.WriteLine("Enter a radius: ");
+                try
+                {
+                    round.Radius = int.Parse(Console.ReadLine());
+
+                    break;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("The area equals: {0}", round.Area);
+            Console.WriteLine("The length equals: {0}", round.Length);
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.Round/Round.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Epam.Task3.Round
+{
+    public class Round
+    {
+        private int radius;
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Radius should be positive");
+                }
+
+                radius = value;
+            }
+        }
+
+        public double Area => Math.PI * Radius * Radius;
+
+        public double Length => 2 * Math.PI * Radius;
+    }
+}
