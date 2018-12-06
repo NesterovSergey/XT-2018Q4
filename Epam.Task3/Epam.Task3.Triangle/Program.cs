10c61241c5986365cc34ba32b061f9abd9f3a380
Sergey Nesterov <RL666@bk.ru>
2018-12-06 04:49:36 +0400

    Added Epam.Task3.Triangle

---
+++ Epam.Task3/Epam.Task3.Triangle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Epam.Task3.Triangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Triangle triangle;
+            int a;
+            int b;
+            int c;
+
+            while (true)
+            {
+                Console.WriteLine("Enter A side: ");
+                a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                {
+                    Console.WriteLine($"A equals or less than 0 {Environment.NewLine}");
+                    continue;
+                }
+
+                Console.WriteLine("Enter B side: ");
+                b = int.Parse(Console.ReadLine());
+                if (b <= 0)
+                {
+                    Console.WriteLine($"B equals or less than 0 {Environment.NewLine}");
+                    continue;
+                }
+
+                Console.WriteLine("Enter C side: ");
+                c = int.Parse(Console.ReadLine());
+                if (c <= 0)
+                {
+                    Console.WriteLine($"C equals or less than 0 {Environment.NewLine}");
+                    continue;
+                }
+
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    triangle = new Triangle(a, b, c);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("the sum of any two sides have to be greater than the third");
+                }
+            }
+
+            Console.WriteLine("The area equals = {0}", triangle.Area);
+            Console.WriteLine("The perimeter equals = {0}", triangle.Perimeter);
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.Triangle/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.Triangle
+{
+    public class Triangle
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int perimeter;
+        private double area;
+
+        public Triangle() : this(1, 1, 1)
+        {
+        }
+
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+            perimeter = a + b + c;
+            area = GetArea(perimeter);
+        }
+
+        public int A => a;
+
+        public int B => b;
+
+        public int C => c;
+
+        public int Perimeter => perimeter;
+
+        public double Area => area;
+
+        private double GetArea(int perimeter)
+        {
+            double semiPerimeter = perimeter / 2d;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+        }
+    }
+}
