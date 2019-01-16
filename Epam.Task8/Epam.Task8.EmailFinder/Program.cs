5fe6e8c28a27179a9060ee9631718fb971128631
Sergey Nesterov <RL666@bk.ru>
2019-01-16 23:02:03 +0400

    Added Epam.Task8.EmailFinder and solution

---
+++ Epam.Task8/Epam.Task8.EmailFinder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Epam.Task8.EmailFinder
+{
+    public class Program
+    {
+        private static readonly string RegString = @"\b(\w+|-|,)@\w+\.[a-z]{2,6}\b";
+
+        public static void Main(string[] args)
+        {
+            ConsoleWriteLine("Epam.Task8.EmailFinder");
+            ConsoleWriteLine(string.Empty);
+
+            EmailFinder();
+        }
+
+        public static void EmailFinder()
+        {
+            Regex regex = new Regex(RegString, RegexOptions.IgnoreCase);
+            string mail;
+
+            ConsoleWriteLine("Enter a text where you want to find email addresses or enter exit to exit");
+            while (true)
+            {
+                mail = ConsoleReadLine();
+
+                if (mail.ToLower().Equals("exit"))
+                {
+                    break;
+                }
+
+                foreach (var match in regex.Matches(mail))
+                {
+                    ConsoleWriteLine(match.ToString());
+                }
+            }
+        }
+
+        public static string ConsoleReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void ConsoleWriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
