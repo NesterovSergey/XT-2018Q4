f2927c0312c2eb6539dae0e1f9f0d54876beb0aa
Sergey Nesterov <RL666@bk.ru>
2019-01-16 23:01:23 +0400

    Added Epam.Task8.NumberValidator

---
+++ Epam.Task8/Epam.Task8.NumberValidator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Epam.Task8.NumberValidator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleWriteLine("Epam.Task8.NumberValidator");
+            ConsoleWriteLine(string.Empty);
+
+            NumberValidation();
+        }
+
+        public static void NumberValidation()
+        {
+            string number;
+
+            ConsoleWriteLine("Please, enter a number you want to check for notation or enter exit to exit");
+            while (true)
+            {
+                number = ConsoleReadLine();
+
+                if (RegularExpression.UsualCheck(number))
+                {
+                    ConsoleWriteLine("The number is in usual notation");
+                }
+                else if (RegularExpression.ScientificCheck(number))
+                {
+                    ConsoleWriteLine("The number is in scientific notation");
+                }
+                else if (number.ToLower().Equals("exit"))
+                {
+                    ConsoleWriteLine("Good luck!");
+                    break;
+                }
+                else
+                {
+                    ConsoleWriteLine("Incorrect data");
+                }
+            }
+        }
+
+        public static string ConsoleReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void ConsoleWriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ Epam.Task8/Epam.Task8.NumberValidator/RegularExpression.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Epam.Task8.NumberValidator
+{
+    public static class RegularExpression
+    {
+        private static readonly string UsualRegString = @"^(\s+)?-?\d+(\.\d+)?(\s+)?$"; 
+        private static readonly string ScientificRegString = @"^(\s+)?-?\d+(\.\d+)?e-?\d+(\s+)?$";
+        private static Regex usualNumberRegex;
+        private static Regex scientificNumberRegex;
+
+        static RegularExpression()
+        {
+            usualNumberRegex = new Regex(UsualRegString);
+            scientificNumberRegex = new Regex(ScientificRegString);
+        }
+
+        public static bool ScientificCheck(string line)
+        {
+            return scientificNumberRegex.IsMatch(line);
+        }
+
+        public static bool UsualCheck(string line)
+        {
+            return usualNumberRegex.IsMatch(line);
+        }
+    }
+}
